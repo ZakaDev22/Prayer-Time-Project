78c21827beb34f04624f7e5b805b4459b0d2fb4c
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-04-15 20:15:29 +0100

    refactor: improve alert handling and update city fetching logic in main.js

--- main.js
+++ main.js
@@ -1,39 +1,54 @@
-
-const div = document.createElement("div");
-div.classList.add(
+const AllertDiv = document.createElement("div");
+AllertDiv.classList.add(
   "alert",
   "alert-danger",
   "position-fixed",
-  "top-0",
+  "top-50",
   "start-50",
-  "translate-middle-x",
+  "translate-middle",
   "w-50",
-  "z-3",
+  "text-center",
+  "shadow",
   "d-none"
 );
-div.innerHTML = "Please Look Into The Console Log for More Details";
+AllertDiv.innerHTML = "Please Look Into The Console Log for More Details";
 const body = document.querySelector("body");
-body.appendChild(div);
+body.appendChild(AllertDiv);
 
+const citiesApi =
+  "https://habous-prayer-times-api.onrender.com/api/v1/available-cities";
 function FillTheSelectWithTheCities() {
   axios
-    .get("https://habous-prayer-times-api.onrender.com/api/v1/available-cities")
+    .get(citiesApi)
     .then((response) => {
-      div.classList.add("d-none"); // Hide the alert if the request is successful
-      const cities = response.data;
+      console.log(response.data);
+      AllertDiv.classList.add("d-none"); // Hide the alert if the request is successful
+      const cities = response.data.cities;
       const select = document.getElementById("citySelect");
       cities.forEach((city) => {
         let option = document.createElement("option");
         option.value = city.id;
         option.text = city.arabicCityName;
+        if (city.id === "104") {
+          option.selected = true; // Set Marrakesh as the default selected option
+        }
         select.add(option);
       });
     })
     .catch((error) => {
       console.error("Error fetching cities:", error);
-      div.classList.remove("d-none"); // Make the alert visible
+      showError("Error fetching cities. Please check the console for details.!!!");
     });
 }
+
+function showError(message) {
+  AllertDiv.innerHTML = message;
+  AllertDiv.classList.remove("d-none");
+  setTimeout(() => {
+    AllertDiv.classList.add("d-none");
+  }, 5000);
+}
+
 
 document.addEventListener("DOMContentLoaded", () => {
   FillTheSelectWithTheCities();
