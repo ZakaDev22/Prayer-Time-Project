48e4147f3816594a56a29c8a498f0584db95182d
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-04-15 19:29:59 +0100

    feat: update index.html and main.js for city selection and prayer times display; add hidden class in style.css

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 import axios from "./node_modules/axios/dist/axios.min.js";
 export default axios;
 
-let div = document.createElement("div");
-div.classList.add("alert alert-danger");
-div.innerHTML ="Error fetching cities. Please try again later. Please Look at the console for more details.";
+// let div = document.createElement("div");
+// div.classList.add("hidden alert alert-danger");
+// div.innerHTML ="Error fetching cities. Please try again later. Please Look at the console for more details.";
+// // div.classList.add("hidden");
+// document.body.appendChild(div);
 
-div.classList.add("hidden");
 function FillTheSelectWithTheCities() {
-  axios
-    .get("https://habous-prayer-times-api.onrender.com/api/v1/available-cities")
-    .then((data) => {
-      div.classList.add("alert alert-danger");
-      div.classList.remove("hidden");
+  axios.Get("https://habous-prayer-times-api.onrender.com/api/v1/available-cities")
+    .then(data => {
+    //   div.classList.add("hidden");
       const select = document.getElementById("citySelect");
       data.forEach((city) => {
         let option = document.createElement("option");
         option.value = city.id;
-        option.text = city.name;
+        option.text = city.arabicCityName;
         select.add(option);
       });
     })
     .catch((error) => {
-      div.classList.remove("hidden");
+    //   div.classList.remove("hidden");
       console.error("Error fetching cities:", error);
     });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    FillTheSelectWithTheCities();
+
+});
