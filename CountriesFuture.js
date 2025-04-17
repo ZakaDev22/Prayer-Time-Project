2e1b8b9bdb51c21685749d299be5fd2f7152d081
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-04-17 14:44:45 +0100

    feat: add functionality to find city prayer times and update navigation link

---
+++ CountriesFuture.js
@@ -0,0 +1,10 @@
+
+const WorldPrayerTimeAPI = "https://api.aladhan.com/v1/timingsByCity?";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("findCity-link").addEventListener("click", function (event) {
+        event.preventDefault(); // Prevent the default anchor click behavior
+       
+    });
+});
